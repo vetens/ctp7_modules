e350929787140a512bb566b00a7e06bce42d3d22
Mykhailo Dalchenko <mykhailo.dalchenko@cern.ch>
2017-04-28 21:23:57 +0200

    Adding configureVFATs and misc

--- src/optohybrid.cpp
+++ src/optohybrid.cpp
@@ -2,7 +2,7 @@
 #include "vfat_parameters.h"
 #include <unistd.h>
 
-void broadcastWriteLocal(lmdb::txn & rtxn, lmdb::dbi & dbi, std::string oh_number, std::string reg_to_write, uint32_t mask, uint32_t val_to_write, RPCMsg * response) {
+void broadcastWriteLocal(lmdb::txn & rtxn, lmdb::dbi & dbi, std::string oh_number, std::string reg_to_write, uint32_t val_to_write, RPCMsg * response, uint32_t mask = 0xFF000000) {
   std::string reg_basename = "GEM_AMC.OH.OH";
   reg_basename += oh_number;
   reg_basename += ".GEB.Broadcast";
@@ -34,28 +34,35 @@ void broadcastWrite(const RPCMsg *request, RPCMsg *response) {
   auto dbi = lmdb::dbi::open(rtxn, nullptr);
   std::string reg_to_write = request->get_string("reg_name");
   uint32_t val_to_write = request->get_word("value");
-  uint32_t mask = request->get_word("mask");
+  uint32_t mask = request->get_key_exists("mask")?request->get_word("mask"):0xFF000000;
   std::string oh_number = request->get_string("oh_number");
-  broadcastWriteLocal(rtxn, dbi, oh_number, reg_to_write, mask, val_to_write, response);
+  broadcastWriteLocal(rtxn, dbi, oh_number, reg_to_write, val_to_write, response, mask);
   rtxn.abort();
 }
 
-void loadVT1(const RPCMsg *request, RPCMsg *response) {
-  auto env = lmdb::env::create();
-  env.set_mapsize(1UL * 1024UL * 1024UL * 40UL); /* 40 MiB */
-  env.open("/mnt/persistent/texas/address_table.mdb", 0, 0664);
-  auto rtxn = lmdb::txn::begin(env, nullptr, MDB_RDONLY);
-  auto dbi = lmdb::dbi::open(rtxn, nullptr);
-  std::string reg_basename = "GEM_AMC.OH.OH";
-  std::string oh_number = request->get_string("oh_number");
-  reg_basename += oh_number;
-  std::string config_file = "";
-  uint32_t vfatN, vt1, trimRange;
-  std::string line, regName;
+// Set default values to VFAT parameters. VFATs will remain in sleep mode
+void biasAllVFATsLocal(lmdb::txn & rtxn, lmdb::dbi & dbi, std::string oh_number, RPCMsg *response, uint32_t mask = 0xFF000000) {
+  for (auto & it:vfat_parameters)
+  {
+    broadcastWriteLocal(rtxn, dbi, oh_number, it.first, it.second, response, mask);
+  }
+}
 
+void setAllVFATsToRunModeLocal(lmdb::txn & rtxn, lmdb::dbi & dbi, std::string oh_number, RPCMsg * response, uint32_t mask = 0xFF000000) {
+  broadcastWriteLocal(rtxn, dbi, oh_number, "ContReg0", 0x37, response, mask);
+}
+
+void setAllVFATsToSleepModeLocal(lmdb::txn & rtxn, lmdb::dbi & dbi, std::string oh_number, RPCMsg * response, uint32_t mask = 0xFF000000) {
+  broadcastWriteLocal(rtxn, dbi, oh_number, "ContReg0", 0x36, response, mask);
+}
+
+void loadVT1Local(lmdb::txn & rtxn, lmdb::dbi & dbi, std::string oh_number, std::string config_file, RPCMsg * response, uint32_t vt1 = 0x64) {
+  std::string reg_basename = "GEM_AMC.OH.OH" + oh_number;
+  uint32_t vfatN, trimRange;
+  std::string line, regName;
   // Check if there's a config file. If yes, set the thresholds and trim range according to it, otherwise st only thresholds (equal on all chips) to provided vt1 value
-  if (request->get_key_exists("vfat_config_filename")) {
-    config_file = request->get_string("vfat_config_filename");//"/mnt/persistent/texas/test/chConfig_GEMINIm01L1.txt";
+  if (config_file!="") {
+    LOGGER->log_message(LogManager::INFO, stdsprintf("CONFIG FILE FOUND: %s", config_file.c_str()));
     std::ifstream infile(config_file);
     std::getline(infile,line);// skip first line
     while (std::getline(infile,line))
@@ -67,28 +74,34 @@ void loadVT1(const RPCMsg *request, RPCMsg *response) {
         break;
       } else {
         regName = reg_basename + ".GEB.VFATS.VFAT" + std::to_string(vfatN)+".VThreshold1";
+        //LOGGER->log_message(LogManager::INFO, stdsprintf("WRITING 0x%8x to REG: %s", vt1, regName.c_str()));
         writeRawReg(rtxn, dbi, regName, vt1, response);
         regName = reg_basename + ".GEB.VFATS.VFAT" + std::to_string(vfatN)+".ContReg3";
+        //LOGGER->log_message(LogManager::INFO, stdsprintf("WRITING 0x%8x to REG: %s", trimRange, regName.c_str()));
         writeRawReg(rtxn, dbi, regName, trimRange, response);
       }
     }
   } else {
-    vt1 = request->get_word("vt1");
-    broadcastWriteLocal(rtxn, dbi, oh_number, "VThreshold1", 0xff000000, vt1, response);
+    LOGGER->log_message(LogManager::INFO, "CONFIG FILE NOT FOUND");
+    broadcastWriteLocal(rtxn, dbi, oh_number, "VThreshold1", vt1, response);
   }
-  rtxn.abort();
 }
 
-void loadTRIMDAC(const RPCMsg *request, RPCMsg *response) {
+void loadVT1(const RPCMsg *request, RPCMsg *response) {
   auto env = lmdb::env::create();
   env.set_mapsize(1UL * 1024UL * 1024UL * 40UL); /* 40 MiB */
   env.open("/mnt/persistent/texas/address_table.mdb", 0, 0664);
   auto rtxn = lmdb::txn::begin(env, nullptr, MDB_RDONLY);
   auto dbi = lmdb::dbi::open(rtxn, nullptr);
-  std::string reg_basename = "GEM_AMC.OH.OH";
   std::string oh_number = request->get_string("oh_number");
-  reg_basename += oh_number;
-  std::string config_file = request->get_string("trim_config_filename");//"/mnt/persistent/texas/test/chConfig_GEMINIm01L1.txt";
+  std::string config_file = request->get_key_exists("thresh_config_filename")?request->get_string("thresh_config_filename"):"";
+  uint32_t vt1 = request->get_key_exists("vt1")?request->get_word("vt1"):0x64;
+  loadVT1Local(rtxn, dbi, oh_number, config_file, response, vt1);
+  rtxn.abort();
+}
+
+void loadTRIMDACLocal(lmdb::txn & rtxn, lmdb::dbi & dbi, std::string oh_number, std::string config_file, RPCMsg * response) {
+  std::string reg_basename = "GEM_AMC.OH.OH" + oh_number + ".GEB.VFATS.VFAT";
   std::ifstream infile(config_file);
   std::string line, regName;
   uint32_t vfatN, vfatCH, trim, mask;
@@ -101,10 +114,41 @@ void loadTRIMDAC(const RPCMsg *request, RPCMsg *response) {
       response->set_string("error", "Error reading settings");
       break;
     } else {
-      regName = reg_basename + ".GEB.VFATS.VFAT" + std::to_string(vfatN)+".VFATChannels.ChanReg" + std::to_string(vfatCH);
+      regName = reg_basename + std::to_string(vfatN)+".VFATChannels.ChanReg" + std::to_string(vfatCH);
       writeRawReg(rtxn, dbi, regName, trim + 32*mask, response);
     }
   }
+}
+
+void loadTRIMDAC(const RPCMsg *request, RPCMsg *response) {
+  auto env = lmdb::env::create();
+  env.set_mapsize(1UL * 1024UL * 1024UL * 40UL); /* 40 MiB */
+  env.open("/mnt/persistent/texas/address_table.mdb", 0, 0664);
+  auto rtxn = lmdb::txn::begin(env, nullptr, MDB_RDONLY);
+  auto dbi = lmdb::dbi::open(rtxn, nullptr);
+  std::string oh_number = request->get_string("oh_number");
+  std::string config_file = request->get_string("trim_config_filename");//"/mnt/persistent/texas/test/chConfig_GEMINIm01L1.txt";
+  loadTRIMDACLocal(rtxn, dbi, oh_number, config_file, response);
+  rtxn.abort();
+}
+
+void configureVFATs(const RPCMsg *request, RPCMsg *response) {
+  auto env = lmdb::env::create();
+  env.set_mapsize(1UL * 1024UL * 1024UL * 40UL); /* 40 MiB */
+  env.open("/mnt/persistent/texas/address_table.mdb", 0, 0664);
+  auto rtxn = lmdb::txn::begin(env, nullptr, MDB_RDONLY);
+  auto dbi = lmdb::dbi::open(rtxn, nullptr);
+  std::string oh_number = request->get_string("oh_number");
+  std::string trim_config_file = request->get_string("trim_config_filename");//"/mnt/persistent/texas/test/chConfig_GEMINIm01L1.txt";
+  std::string thresh_config_file = request->get_key_exists("thresh_config_filename")?request->get_string("thresh_config_filename"):"";
+  uint32_t vt1 = request->get_key_exists("vt1")?request->get_word("vt1"):0x64;
+  LOGGER->log_message(LogManager::INFO, "BIAS VFATS");
+  biasAllVFATsLocal(rtxn, dbi, oh_number, response);
+  LOGGER->log_message(LogManager::INFO, "LOAD VT1 VFATS");
+  loadVT1Local(rtxn, dbi, oh_number, thresh_config_file, response, vt1);
+  LOGGER->log_message(LogManager::INFO, "LOAD TRIM VFATS");
+  loadTRIMDACLocal(rtxn, dbi, oh_number, trim_config_file, response);
+  if (request->get_key_exists("set_run")) setAllVFATsToRunModeLocal(rtxn, dbi, oh_number, response);
   rtxn.abort();
 }
 
@@ -120,5 +164,6 @@ extern "C" {
 		modmgr->register_method("optohybrid", "broadcastWrite", broadcastWrite);
 		modmgr->register_method("optohybrid", "loadVT1", loadVT1);
 		modmgr->register_method("optohybrid", "loadTRIMDAC", loadTRIMDAC);
+		modmgr->register_method("optohybrid", "configureVFATs", configureVFATs);
 	}
 }
