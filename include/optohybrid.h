3b8a17a35fe6859aeb85e668a612d52293896aa2
Brian Dorney <brian.l.dorney@cern.ch>
2018-08-27 09:28:57 +0200

    functions in header file now in alphabetical order

--- include/optohybrid.h
+++ include/optohybrid.h
@@ -11,6 +11,14 @@
 #include "vfat_parameters.h"
 #include <unistd.h>
 
+/*! \fn void biasAllVFATsLocal(localArgs * la, uint32_t ohN, uint32_t mask = 0xFF000000)
+ *  \brief Local callable. Sets default values to VFAT parameters. VFATs will remain in sleep mode
+ *  \param la Local arguments structure
+ *  \param ohN Optohybrid optical link number (string)
+ *  \param mask VFAT mask. Default: no chips will be masked
+ */
+void biasAllVFATsLocal(localArgs * la, uint32_t ohN, uint32_t mask = 0xFF000000);
+
 /*! \fn void broadcastWriteLocal(localArgs * la, uint32_t ohN, std::string regName, uint32_t value, uint32_t mask = 0xFF000000)
  *  \brief Local callable version of broadcastWrite
  *  \param la Local arguments structure
@@ -46,69 +54,6 @@ void broadcastReadLocal(localArgs * la, uint32_t *outData, uint32_t ohN, std::st
  */
 void broadcastRead(const RPCMsg *request, RPCMsg *response);
 
-/*! \fn void biasAllVFATsLocal(localArgs * la, uint32_t ohN, uint32_t mask = 0xFF000000)
- *  \brief Local callable. Sets default values to VFAT parameters. VFATs will remain in sleep mode
- *  \param la Local arguments structure
- *  \param ohN Optohybrid optical link number (string)
- *  \param mask VFAT mask. Default: no chips will be masked
- */
-void biasAllVFATsLocal(localArgs * la, uint32_t ohN, uint32_t mask = 0xFF000000);
-
-/*! \fn void setAllVFATsToRunModeLocal(localArgs * la, uint32_t ohN, uint32_t mask = 0xFF000000)
- *  \brief Local callable. Sets VFATs to run mode
- *  \param la Local arguments structure
- *  \param ohN Optohybrid optical link number (string)
- *  \param mask VFAT mask. Default: no chips will be masked
- */
-void setAllVFATsToRunModeLocal(localArgs * la, uint32_t ohN, uint32_t mask = 0xFF000000);
-
-/*! \fn void setAllVFATsToSleepModeLocal(localArgs * la, uint32_t ohN, uint32_t mask = 0xFF000000)
- *  \brief Local callable. Sets VFATs to sleep mode
- *  \param la Local arguments structure
- *  \param ohN Optohybrid optical link number (string)
- *  \param mask VFAT mask. Default: no chips will be masked
- */
-void setAllVFATsToSleepModeLocal(localArgs * la, uint32_t ohN, uint32_t mask = 0xFF000000);
-
-/*! \fn void loadVT1Local(localArgs * la, uint32_t ohN, std::string config_file, uint32_t vt1 = 0x64)
- *  \brief Local callable version of loadVT1
- *  \param la Local arguments structure
- *  \param ohN Optohybrid optical link number
- *  \param config_file Configuration file with VT1 and trim values. Optional (could be supplied as an empty string)
- *  \param vt1. Default: 0x64, used if the config_file is not provided
- *  \return Bitmask of sync'ed VFATs
- */
-void loadVT1Local(localArgs * la, uint32_t ohN, std::string config_file, uint32_t vt1 = 0x64);
-
-/*! \fn void loadVT1(const RPCMsg *request, RPCMsg *response)
- *  \brief Sets threshold and trim range for each VFAT2 chip
- *  \param request RPC response message
- *  \param response RPC response message
- */
-void loadVT1(const RPCMsg *request, RPCMsg *response);
-
-/*! \fn void loadTRIMDACLocal(localArgs * la, uint32_t ohN, std::string config_file)
- *  \brief Local callable version of loadTRIMDAC
- *  \param la Local arguments structure
- *  \param ohN Optohybrid optical link number
- *  \param config_file Configuration file with trimming parameters
- */
-void loadTRIMDACLocal(localArgs * la, uint32_t ohN, std::string config_file);
-
-/*! \fn void loadTRIMDAC(const RPCMsg *request, RPCMsg *response)
- *  \brief Sets trimming DAC parameters for each channel of each chip
- *  \param request RPC response message
- *  \param response RPC response message
- */
-void loadTRIMDAC(const RPCMsg *request, RPCMsg *response);
-
-/*! \fn void configureVFATs(const RPCMsg *request, RPCMsg *response)
- *  \brief Configures VFAT chips (V2B only). Calls biasVFATs, loads VT1 and TRIMDAC values from the config files
- *  \param request RPC response message
- *  \param response RPC response message
- */
-void configureVFATs(const RPCMsg *request, RPCMsg *response);
-
 /*! \fn configureScanModuleLocal(localArgs * la, uint32_t ohN, uint32_t vfatN, uint32_t scanmode, bool useUltra, uint32_t mask, uint32_t ch, uint32_t nevts, uint32_t dacMin, uint32_t dacMax, uint32_t dacStep)
  *  \brief Local callable version of configureScanModule
  *
@@ -142,35 +87,12 @@ void configureScanModuleLocal(localArgs * la, uint32_t ohN, uint32_t vfatN, uint
  */
 void configureScanModule(const RPCMsg *request, RPCMsg *response);
 
-/*! \fn void printScanConfigurationLocal(localArgs * la, uint32_t ohN, bool useUltra)
- *  \brief Local callable version of printScanConfiguration
- *  \param la Local arguments structure
- *  \param ohN Optohybrid optical link number
- *  \param useUltra Set to 1 in order to use the ultra scan
- */
-void printScanConfigurationLocal(localArgs * la, uint32_t ohN, bool useUltra);
-
-/*! \fn void printScanConfiguration(const RPCMsg *request, RPCMsg *response)
- *  \brief Prints V2b FW scan module configuration
+/*! \fn void configureVFATs(const RPCMsg *request, RPCMsg *response)
+ *  \brief Configures VFAT chips (V2B only). Calls biasVFATs, loads VT1 and TRIMDAC values from the config files
  *  \param request RPC response message
  *  \param response RPC response message
  */
-void printScanConfiguration(const RPCMsg *request, RPCMsg *response);
-
-/*! \fn void startScanModuleLocal(localArgs * la, uint32_t ohN, bool useUltra)
- *  \brief Local callable version of startScanModule
- *  \param la Local arguments structure
- *  \param ohN Optohybrid optical link number
- *  \param useUltra Set to 1 in order to use the ultra scan
- */
-void startScanModuleLocal(localArgs * la, uint32_t ohN, bool useUltra);
-
-/*! \fn void startScanModule(const RPCMsg *request, RPCMsg *response)
- *  \brief Starts V2b FW scan module
- *  \param request RPC response message
- *  \param response RPC response message
- */
-void startScanModule(const RPCMsg *request, RPCMsg *response);
+void configureVFATs(const RPCMsg *request, RPCMsg *response);
 
 /*! \fn void getUltraScanResultsLocal(localArgs *la, uint32_t *outData, uint32_t ohN, uint32_t nevts, uint32_t dacMin, uint32_t dacMax, uint32_t dacStep)
  *  \brief Local callable version of getUltraScanResults
@@ -190,6 +112,98 @@ void getUltraScanResultsLocal(localArgs *la, uint32_t *outData, uint32_t ohN, ui
  */
 void getUltraScanResults(const RPCMsg *request, RPCMsg *response);
 
+/*! \fn void loadTRIMDACLocal(localArgs * la, uint32_t ohN, std::string config_file)
+ *  \brief Local callable version of loadTRIMDAC
+ *  \param la Local arguments structure
+ *  \param ohN Optohybrid optical link number
+ *  \param config_file Configuration file with trimming parameters
+ */
+void loadTRIMDACLocal(localArgs * la, uint32_t ohN, std::string config_file);
+
+/*! \fn void loadTRIMDAC(const RPCMsg *request, RPCMsg *response)
+ *  \brief Sets trimming DAC parameters for each channel of each chip
+ *  \param request RPC response message
+ *  \param response RPC response message
+ */
+void loadTRIMDAC(const RPCMsg *request, RPCMsg *response);
+
+/*! \fn void loadVT1Local(localArgs * la, uint32_t ohN, std::string config_file, uint32_t vt1 = 0x64)
+ *  \brief Local callable version of loadVT1
+ *  \param la Local arguments structure
+ *  \param ohN Optohybrid optical link number
+ *  \param config_file Configuration file with VT1 and trim values. Optional (could be supplied as an empty string)
+ *  \param vt1. Default: 0x64, used if the config_file is not provided
+ *  \return Bitmask of sync'ed VFATs
+ */
+void loadVT1Local(localArgs * la, uint32_t ohN, std::string config_file, uint32_t vt1 = 0x64);
+
+/*! \fn void loadVT1(const RPCMsg *request, RPCMsg *response)
+ *  \brief Sets threshold and trim range for each VFAT2 chip
+ *  \param request RPC response message
+ *  \param response RPC response message
+ */
+void loadVT1(const RPCMsg *request, RPCMsg *response);
+
+/*! \fn void printScanConfigurationLocal(localArgs * la, uint32_t ohN, bool useUltra)
+ *  \brief Local callable version of printScanConfiguration
+ *  \param la Local arguments structure
+ *  \param ohN Optohybrid optical link number
+ *  \param useUltra Set to 1 in order to use the ultra scan
+ */
+void printScanConfigurationLocal(localArgs * la, uint32_t ohN, bool useUltra);
+
+/*! \fn void printScanConfiguration(const RPCMsg *request, RPCMsg *response)
+ *  \brief Prints V2b FW scan module configuration
+ *  \param request RPC response message
+ *  \param response RPC response message
+ */
+void printScanConfiguration(const RPCMsg *request, RPCMsg *response);
+
+/*! \fn void setAllVFATsToRunModeLocal(localArgs * la, uint32_t ohN, uint32_t mask = 0xFF000000)
+ *  \brief Local callable. Sets VFATs to run mode
+ *  \param la Local arguments structure
+ *  \param ohN Optohybrid optical link number (string)
+ *  \param mask VFAT mask. Default: no chips will be masked
+ */
+void setAllVFATsToRunModeLocal(localArgs * la, uint32_t ohN, uint32_t mask = 0xFF000000);
+
+/*! \fn void setAllVFATsToSleepModeLocal(localArgs * la, uint32_t ohN, uint32_t mask = 0xFF000000)
+ *  \brief Local callable. Sets VFATs to sleep mode
+ *  \param la Local arguments structure
+ *  \param ohN Optohybrid optical link number (string)
+ *  \param mask VFAT mask. Default: no chips will be masked
+ */
+void setAllVFATsToSleepModeLocal(localArgs * la, uint32_t ohN, uint32_t mask = 0xFF000000);
+
+/*! \fn void startScanModuleLocal(localArgs * la, uint32_t ohN, bool useUltra)
+ *  \brief Local callable version of startScanModule
+ *  \param la Local arguments structure
+ *  \param ohN Optohybrid optical link number
+ *  \param useUltra Set to 1 in order to use the ultra scan
+ */
+void startScanModuleLocal(localArgs * la, uint32_t ohN, bool useUltra);
+
+/*! \fn void startScanModule(const RPCMsg *request, RPCMsg *response)
+ *  \brief Starts V2b FW scan module
+ *  \param request RPC response message
+ *  \param response RPC response message
+ */
+void startScanModule(const RPCMsg *request, RPCMsg *response);
+
+/*! \fn void statusOHLocal(localArgs * la, uint32_t ohEnMask)
+ *  \brief Local callable version of statusOH
+ *  \param la Local arguments structure
+ *  \param ohEnMask Bit mask of enabled optical links
+ */
+void statusOHLocal(localArgs * la, uint32_t ohEnMask);
+
+/*! \fn void statusOH(const RPCMsg *request, RPCMsg *response)
+ *  \brief Returns a list of the most important monitoring registers of optohybrids
+ *  \param request RPC response message
+ *  \param response RPC response message
+ */
+void statusOH(const RPCMsg *request, RPCMsg *response);
+
 /*! \fn void stopCalPulse2AllChannelsLocal(localArgs *la, uint32_t ohN, uint32_t mask, uint32_t ch_min, uint32_t ch_max)
  *  \brief Local callable version of stopCalPulse2AllChannels
  *  \param la Local arguments structure
@@ -206,19 +220,5 @@ void stopCalPulse2AllChannelsLocal(localArgs *la, uint32_t ohN, uint32_t mask, u
  *  \param response RPC response message
  */
 void stopCalPulse2AllChannels(const RPCMsg *request, RPCMsg *response);
-
-/*! \fn void statusOHLocal(localArgs * la, uint32_t ohEnMask)
- *  \brief Local callable version of statusOH
- *  \param la Local arguments structure
- *  \param ohEnMask Bit mask of enabled optical links
- */
-void statusOHLocal(localArgs * la, uint32_t ohEnMask);
-
-/*! \fn void statusOH(const RPCMsg *request, RPCMsg *response)
- *  \brief Returns a list of the most important monitoring registers of optohybrids
- *  \param request RPC response message
- *  \param response RPC response message
- */
-void statusOH(const RPCMsg *request, RPCMsg *response);
 
 #endif
