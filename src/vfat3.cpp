96f94e45a4750838b7a8a6330da5a336c284a1de
Andrew Levin <andrew.michael.levin@cern.ch>
2018-10-04 00:51:05 +0200

    remove unnecessary whitespace

--- src/vfat3.cpp
+++ src/vfat3.cpp
@@ -305,7 +305,6 @@ void readVFAT3ADCMultiLink(const RPCMsg *request, RPCMsg *response){
     request->get_word_array("ohVfatMaskArray",ohVfatMaskArray);
     bool useExtRefADC = request->get_word("useExtRefADC");
 
-
     struct localArgs la = {.rtxn = rtxn, .dbi = dbi, .response = response};
     unsigned int NOH = readReg(&la, "GEM_AMC.GEM_SYSTEM.CONFIG.NUM_OF_OH");
     if (request->get_key_exists("NOH")){
