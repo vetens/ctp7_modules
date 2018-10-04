b9559811fb366de1f4d389f4d7c590b85ae83903
Brian Dorney <brian.l.dorney@cern.ch>
2018-10-04 20:01:56 +0200

    getmonOHSysmonLocal() can now read v2b sysmon values

--- src/daq_monitor.cpp
+++ src/daq_monitor.cpp
@@ -4,6 +4,7 @@
  *  \author Brian Dorney <brian.l.dorney@cern.ch>
  */
 
+#include "amc.h"
 #include "daq_monitor.h"
 #include <string>
 #include "utils.h"
@@ -357,66 +358,96 @@ void getmonOHSysmonLocal(localArgs *la, int NOH, int ohMask, bool doReset){
     std::string strKeyName;
     std::string strRegBase;
 
-    for (int ohN = 0; ohN < NOH; ++ohN){ //Loop over all optohybrids
-        // If this Optohybrid is masked skip it
-        if(!((ohMask >> ohN) & 0x1)){
-            continue;
-        }
+    if (fw_version_check("getmonOHSysmon", la) == 3){
+        for (int ohN = 0; ohN < NOH; ++ohN){ //Loop over all optohybrids
+            // If this Optohybrid is masked skip it
+            if(!((ohMask >> ohN) & 0x1)){
+                continue;
+            }
 
-        //Set regBase
-        strRegBase = stdsprintf("GEM_AMC.OH.OH%i.FPGA.ADC.CTRL.",ohN);
+            //Set regBase
+            strRegBase = stdsprintf("GEM_AMC.OH.OH%i.FPGA.ADC.CTRL.",ohN);
 
-        //Log Message
-        LOGGER->log_message(LogManager::INFO, stdsprintf("Reading Sysmon Values for OH%i",ohN));
+            //Log Message
+            LOGGER->log_message(LogManager::INFO, stdsprintf("Reading Sysmon Values for OH%i",ohN));
 
-        //Issue reset??
-        if(doReset){
-            LOGGER->log_message(LogManager::INFO, stdsprintf("Reseting CNT_OVERTEMP, CNT_VCCAUX_ALARM and CNT_VCCINT_ALARM for OH%i",ohN));
-            writeReg(la, strRegBase+"RESET", 0x1);
-        }
+            //Issue reset??
+            if(doReset){
+                LOGGER->log_message(LogManager::INFO, stdsprintf("Reseting CNT_OVERTEMP, CNT_VCCAUX_ALARM and CNT_VCCINT_ALARM for OH%i",ohN));
+                writeReg(la, strRegBase+"RESET", 0x1);
+            }
 
-        //Read Alarm conditions & counters - OVERTEMP
-        strKeyName = stdsprintf("OH%i.OVERTEMP",ohN);
-        la->response->set_word(strKeyName,readReg(la, strRegBase + "OVERTEMP"));
+            //Read Alarm conditions & counters - OVERTEMP
+            strKeyName = stdsprintf("OH%i.OVERTEMP",ohN);
+            la->response->set_word(strKeyName,readReg(la, strRegBase + "OVERTEMP"));
 
-        strKeyName = stdsprintf("OH%i.CNT_OVERTEMP",ohN);
-        la->response->set_word(strKeyName,readReg(la, strRegBase + "CNT_OVERTEMP"));
+            strKeyName = stdsprintf("OH%i.CNT_OVERTEMP",ohN);
+            la->response->set_word(strKeyName,readReg(la, strRegBase + "CNT_OVERTEMP"));
 
-        //Read Alarm conditions & counters - VCCAUX_ALARM
-        strKeyName = stdsprintf("OH%i.VCCAUX_ALARM",ohN);
-        la->response->set_word(strKeyName,readReg(la, strRegBase + "VCCAUX_ALARM"));
+            //Read Alarm conditions & counters - VCCAUX_ALARM
+            strKeyName = stdsprintf("OH%i.VCCAUX_ALARM",ohN);
+            la->response->set_word(strKeyName,readReg(la, strRegBase + "VCCAUX_ALARM"));
 
-        strKeyName = stdsprintf("OH%i.CNT_VCCAUX_ALARM",ohN);
-        la->response->set_word(strKeyName,readReg(la, strRegBase + "CNT_VCCAUX_ALARM"));
+            strKeyName = stdsprintf("OH%i.CNT_VCCAUX_ALARM",ohN);
+            la->response->set_word(strKeyName,readReg(la, strRegBase + "CNT_VCCAUX_ALARM"));
 
-        //Read Alarm conditions & counters - VCCINT_ALARM
-        strKeyName = stdsprintf("OH%i.VCCINT_ALARM",ohN);
-        la->response->set_word(strKeyName,readReg(la, strRegBase + "VCCINT_ALARM"));
+            //Read Alarm conditions & counters - VCCINT_ALARM
+            strKeyName = stdsprintf("OH%i.VCCINT_ALARM",ohN);
+            la->response->set_word(strKeyName,readReg(la, strRegBase + "VCCINT_ALARM"));
 
-        strKeyName = stdsprintf("OH%i.CNT_VCCINT_ALARM",ohN);
-        la->response->set_word(strKeyName,readReg(la, strRegBase + "CNT_VCCINT_ALARM"));
+            strKeyName = stdsprintf("OH%i.CNT_VCCINT_ALARM",ohN);
+            la->response->set_word(strKeyName,readReg(la, strRegBase + "CNT_VCCINT_ALARM"));
 
-        //Enable Sysmon ADC Read
-        writeReg(la, strRegBase + "ENABLE", 0x1);
+            //Enable Sysmon ADC Read
+            writeReg(la, strRegBase + "ENABLE", 0x1);
 
-        //Read Sysmon Values - Core Temperature
-        writeReg(la, strRegBase + "ADR_IN", 0x0);
-        strKeyName = stdsprintf("OH%i.FPGA_CORE_TEMP",ohN);
-        la->response->set_word(strKeyName, ((readReg(la, strRegBase + "DATA_OUT") >> 6) & 0x3ff));
+            //Read Sysmon Values - Core Temperature
+            writeReg(la, strRegBase + "ADR_IN", 0x0);
+            strKeyName = stdsprintf("OH%i.FPGA_CORE_TEMP",ohN);
+            la->response->set_word(strKeyName, ((readReg(la, strRegBase + "DATA_OUT") >> 6) & 0x3ff));
 
-        //Read Sysmon Values - Core Voltage
-        writeReg(la, strRegBase + "ADR_IN", 0x1);
-        strKeyName = stdsprintf("OH%i.FPGA_CORE_1V0",ohN);
-        la->response->set_word(strKeyName, ((readReg(la, strRegBase + "DATA_OUT") >> 6) & 0x3ff));
+            //Read Sysmon Values - Core Voltage
+            writeReg(la, strRegBase + "ADR_IN", 0x1);
+            strKeyName = stdsprintf("OH%i.FPGA_CORE_1V0",ohN);
+            la->response->set_word(strKeyName, ((readReg(la, strRegBase + "DATA_OUT") >> 6) & 0x3ff));
 
-        //Read Sysmon Values - I/O Voltage
-        writeReg(la, strRegBase + "ADR_IN", 0x2);
-        strKeyName = stdsprintf("OH%i.FPGA_CORE_2V5_IO",ohN);
-        la->response->set_word(strKeyName, ((readReg(la, strRegBase + "DATA_OUT") >> 6) & 0x3ff));
+            //Read Sysmon Values - I/O Voltage
+            writeReg(la, strRegBase + "ADR_IN", 0x2);
+            strKeyName = stdsprintf("OH%i.FPGA_CORE_2V5_IO",ohN);
+            la->response->set_word(strKeyName, ((readReg(la, strRegBase + "DATA_OUT") >> 6) & 0x3ff));
 
-        //Disable Sysmon ADC Read
-        writeReg(la, strRegBase + "ENABLE", 0x0);
-    } //End Loop over all optohybrids
+            //Disable Sysmon ADC Read
+            writeReg(la, strRegBase + "ENABLE", 0x0);
+        } //End Loop over all optohybrids
+    } //End Case: v3 Electronics
+    else{ //Case: v2b Electronics
+        for (int ohN = 0; ohN < NOH; ++ohN){ //Loop over all optohybrids
+            // If this Optohybrid is masked skip it
+            if(!((ohMask >> ohN) & 0x1)){
+                continue;
+            }
+
+            //Set regBase
+            strRegBase = stdsprintf("GEM_AMC.OH.OH%i.ADC.",ohN);
+
+            //Log Message
+            LOGGER->log_message(LogManager::INFO, stdsprintf("Reading Sysmon Values for OH%i",ohN));
+
+            //Read Sysmon Values - Core Temperature
+            strKeyName = stdsprintf("OH%i.FPGA_CORE_TEMP",ohN);
+            la->response->set_word(strKeyName, ((readReg(la, strRegBase + "TEMP") >> 6) & 0x3ff));
+
+            //Read Sysmon Values - Core Voltage
+            strKeyName = stdsprintf("OH%i.FPGA_CORE_1V0",ohN);
+            la->response->set_word(strKeyName, ((readReg(la, strRegBase + "VCCINT") >> 6) & 0x3ff));
+
+            //Read Sysmon Values - I/O Voltage
+            strKeyName = stdsprintf("OH%i.FPGA_CORE_2V5_IO",ohN);
+            la->response->set_word(strKeyName, ((readReg(la, strRegBase + "VCCAUX") >> 6) & 0x3ff));
+        } //End Loop all optohybrids
+    } //End Case: v2b Electronics
+
+    return;
 } //End getmonOHSysmonLocal()
 
 void getmonOHSysmon(const RPCMsg *request, RPCMsg *response){
