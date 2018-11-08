e8495dbdcef6d0e82e736946bfd69c02b6d49633
fsimone91 <39649267+fsimone91@users.noreply.github.com>
2018-11-08 16:10:40 +0100

    Update calibration_routines.cpp

--- src/calibration_routines.cpp
+++ src/calibration_routines.cpp
@@ -1474,10 +1474,9 @@ std::vector<uint32_t> dacScanLocal(localArgs *la, uint32_t ohN, uint32_t dacSele
                 continue;
             } //End Case: VFAT is masked, skip
             else{ //Case: VFAT is not masked
-                //Set DAC value
+            //Set DAC value
                 std::string strDacReg = stdsprintf("GEM_AMC.OH.OH%i.GEB.VFAT%i.",ohN,vfatN) + regName;
                 writeReg(la, strDacReg, dacVal);
-
                 for(int i=0; i<100; ++i){ //Read 100 times and take avg value
                 //Read the ADC
                     Temp = readRawAddress(adcAddr[vfatN], la->response);
